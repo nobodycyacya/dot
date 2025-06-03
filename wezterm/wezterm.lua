6e01f48f6ebd33ec6a4102a657b9b1255a070cbf
nobodycyacya <cya.chen.ya@gmail.com>
2025-06-03 13:29:37 +0800

    Update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,27 +2,31 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  config.font_size = 10
-  config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold', italic = false })
-  config.default_prog = { 'C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe' }
-elseif wezterm.target_triple == "aarch64-apple-darwin"  then
-  config.font_size = 11
-  config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold', italic = false })
-  config.default_prog = { '/usr/bin/zsh', '-l' }
-elseif wezterm.target_triple == "x86_64-unknown-linux-gnu"  then
-  config.font_size = 12
-  config.font = wezterm.font('JetBrainsMono Nerd Font', { weight = 'Bold', italic = false })
-  config.default_prog = { '/usr/bin/bash', '-l' }
+	config.font_size = 10
+	config.default_prog = { "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe" }
+	config.window_background_opacity = 0.98
+elseif wezterm.target_triple == "x86_64-apple-darwin" then
+	config.font_size = 11
+	config.default_prog = { "/usr/bin/zsh", "-l" }
+	config.window_background_opacity = 0.98
+elseif wezterm.target_triple == "aarch64-apple-darwin" then
+	config.font_size = 11
+	config.default_prog = { "/usr/bin/zsh", "-l" }
+	config.window_background_opacity = 0.98
+elseif wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+	config.font_size = 12
+	config.default_prog = { "/usr/bin/bash", "-l" }
+	config.window_background_opacity = 1.0
 end
 
+config.cell_width = 0.9
+config.color_scheme = "tokyonight_night"
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold", italic = false })
+config.front_end = "OpenGL"
+config.freetype_load_target = "Light"
+config.freetype_render_target = "HorizontalLcd"
 config.hide_tab_bar_if_only_one_tab = true
 config.line_height = 1.1
-config.color_scheme = 'DoomOne'
-config.window_padding = {
-  left = 12,
-  right = 12,
-  top = 12,
-  bottom = 12
-}
+config.window_close_confirmation = "NeverPrompt"
 
 return config
